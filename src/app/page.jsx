02d660b783ed36bef4c0f7a21f7f192a5ea455ab
Le-vagabondev <75924500+Le-vagabondev@users.noreply.github.com>
2023-10-04 04:44:06 -0500

    alpha2.1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,8 @@ import specific from "./page.module.css";
 import About from "@/components/about/About";
 import React, { useLayoutEffect, useRef,useState } from "react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Lilita_One, Shadows_Into_Light } from 'next/font/google'
+import { ScrollTrigger} from "gsap/ScrollTrigger";
+import { Lilita_One, Shadows_Into_Light } from 'next/font/google';
 gsap.registerPlugin(ScrollTrigger);
 
 const lilita = Lilita_One({
@@ -20,28 +20,41 @@ const shadow = Shadows_Into_Light({
 }) 
 
 export default function Home() {
- 
+  var tl = gsap.timeline({
+    scrollTrigger: { 
+      trigger: ".wrapper",
+      start: "top center",
+      end: "+=800", // end after scrolling 500px beyond the start
+      markers: true,
+      toggleActions:"play pause reverse reset"
+    
+    }
+  });
+
   useLayoutEffect(() => {
-
-    // all your animations go in here...
-gsap.from(".wrapper", {
-x: 1500,
-duration: 1,/*{min: 1, max:1.5}*/
-// autoAlpha:0,
-alpha:0,
-delay: 0.5,
-scrollTrigger: { 
-  trigger: ".wrapper",
-  start: "top center",
-  end: "+=800", // end after scrolling 500px beyond the start
-  markers: true,
-  toggleActions:"play pause reverse reset",
-  ease: "none" 
-
-}// start the animation when ".box" enters the viewport (once)
-
-});
-
+    tl.from(".anim_1",{
+    duration: 0.8,/*{min: 1, max:1.5}*/
+    y:150,
+    scale:0
+    // autoAlpha:0,
+  
+    })
+    tl.from(".anim_2",{
+      duration: 0.8,/*{min: 1, max:1.5}*/
+      x:150,
+      opacity: 0
+      // autoAlpha:0,
+    
+      })
+      tl.from(".anim_3",{
+        duration: 0.8,/*{min: 1, max:1.5}*/
+        y:50,
+        opacity:0
+        // autoAlpha:0,
+      
+        })
+ 
+    
 
 }, []);
   const handlerScroll= () => {
@@ -54,18 +67,18 @@ scrollTrigger: {
   }
   return (
     <div >
-      <span className={ `${styles.grid}`}>
+      <span className={ `${styles.grid} `}>
     <span className={ ``}>
       <p className={specific.welcome}> </p>
     </span>
      < span className={`${styles.grid__title }`}>
-     <p className={`${specific.size3} ${specific.line} ${shadow.className}`}> Hey, <br /> My name is  </p>
+     <p className={`${specific.size2} ${specific.line} `}> Hey, <br /> My name is  </p>
    </span>
    <span className={ `${styles.grid__name }`}>
       <h1 className={`${specific.size6} ${lilita.className} `}>Zander</h1>
     </span>
-     <span className={ `${styles.grid__subheader } ${shadow.className}`}>
-     <p className={`${specific.size4} `}>I build things ****** ******* for the web</p>
+     <span className={ `${styles.grid__subheader }`}>
+     <p className={`${specific.size3} `}>I build things  for the web</p>
    </span>
 
    <span className={ `${styles.grid__read }`}>
